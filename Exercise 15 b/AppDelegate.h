97f7755c4754d2d80664fad0c1c25a901ff38f19
Eugen Bopp <peugens@bubu.fritz.box>
2016-12-19 19:28:10 +0100

    Initial Commit

---
+++ Exercise 15 b/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Exercise 15 b
+//
+//  Created by Eugen Bopp on 19/12/2016.
+//  Copyright © 2016 Eugen Bopp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
